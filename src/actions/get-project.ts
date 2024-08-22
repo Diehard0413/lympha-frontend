4c520eafb54f8f463389add87a43772d10099c1a
goldeneagle1035 <goldeneagle1035@gmail.com>
2024-08-22 19:00:08 +0200

    adjust fetching projects

--- src/actions/get-project.ts
+++ src/actions/get-project.ts
@@ -7,8 +7,10 @@ export const getAllProjects = async () => {
   try {
     const projects = await prisma.project.findMany();
 
+    console.log(projects);
     return { projects };
   } catch (error) {
+    console.log(error);
     return { projects: [] };
   }
 };
